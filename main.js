11bb4a2de5b5085bb1c330d9f8ac01194b716c4b
EldarHasanli-maker <76447592+EldarHasanli-maker@users.noreply.github.com>
2021-09-19 11:12:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+img="";
+status="";
+objects=[];
+function preload(){
+    img=loadImage("dog_cat.jpg");
+}
+function setup(){
+canvas=createCanvas(380,380);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(380,380);
+video.hide();
+objectdetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="Status: detecting objects";
+
+}
+function draw(){
+    image(video,0,0,380,380);
+   if(status!=""){
+    objectdetector.detect(video,gotResult);
+    r=random(255);
+    g=random(255);
+    b=random(255);
+for(i=0;i<objects.length;i++){
+    document.getElementById("status").innerHTML="Status: objects detected";
+    document.getElementById("number_of_objects").innerHTML="Number of objects detected are:"+objects.length;
+    fill(r,g,b);
+    percent=floor(objects[i].confidence*100);
+    text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
+    noFill();
+    stroke(r,g,b);
+    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+}
+} 
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+}
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    objects=results;
+}
+}
